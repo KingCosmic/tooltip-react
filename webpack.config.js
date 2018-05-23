bbc65cb64b3b3a4b9dfc94b78e581de3c4da50e1
Austyn Studdard <KingCosmic2016@gmail.com>
2018-05-23 15:03:35 -0500

    changing webpack build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,83 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: path.join(__dirname, 'src/docs/'),
-  output: {
-    path: path.join(__dirname, 'docs'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]_[local]_[hash:base64]',
-              sourceMap: true,
-              minimize: true
-            }
+module.exports = [
+  {
+    entry: path.join(__dirname, 'src/docs'),
+    output: {
+      path: path.join(__dirname, 'docs'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
           }
-        ]
-      }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]_[local]_[hash:base64]',
+                sourceMap: true,
+                minimize: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src/docs/index.html')
+      })
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/docs/index.html')
-    })
-  ]
-};
+  
+  {
+    entry: path.join(__dirname, 'src/lib'),
+    output: {
+      path: path.join(__dirname, 'lib'),
+      filename: 'index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]_[local]_[hash:base64]',
+                sourceMap: true,
+                minimize: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+];
