cb50c14299d77705a2b26d8800ec6baa7660bd37
Austyn Studdard <KingCosmic2016@gmail.com>
2018-05-23 15:42:52 -0500

    changed setup for builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,44 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = [
-  {
-    entry: path.join(__dirname, 'src/docs'),
-    output: {
-      path: path.join(__dirname, 'docs'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'style-loader'
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[name]_[local]_[hash:base64]',
-                sourceMap: true,
-                minimize: true
-              }
-            }
-          ]
+module.exports = {
+  entry: path.join(__dirname, 'src/docs'),
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'src/docs/index.html')
-      })
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
     ]
   },
-  
-  {
-    entry: path.join(__dirname, 'src/lib'),
-    output: {
-      path: path.join(__dirname, 'lib'),
-      filename: 'index.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'style-loader'
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[name]_[local]_[hash:base64]',
-                sourceMap: true,
-                minimize: true
-              }
-            }
-          ]
-        }
-      ]
-    }
-  }
-];
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/docs/index.html')
+    })
+  ]
+};
